7dcf4286d012b5eebaaec87a0884c136a206d1ae
0220832211005 <0220832211005@LAB4MAQ26>
2023-03-22 11:05:51 -0300

    construindo cadrasto e lista de usuarios

--- webApp_manha/Controllers/HomeController.cs
+++ webApp_manha/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace webApp_manha.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
