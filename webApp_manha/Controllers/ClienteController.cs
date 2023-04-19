c7bc7a375adbe72d3eb6cd9aec6f6bf08e37bc81
0220832211005 <0220832211005@LAB4MAQ26>
2023-04-19 11:14:40 -0300

    Criada as funções de adicionar, excluir, e salvar

--- webApp_manha/Controllers/ClienteController.cs
+++ webApp_manha/Controllers/ClienteController.cs
@@ -1,13 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using webApp_manha.Models;
 
 namespace webApp_manha.Controllers
 {
     public class ClienteController : Controller
     {
+
+        public static List<ClienteViewModel> lista = new List<ClienteViewModel>();
         public IActionResult Lista()
-        {
-            return View();
+        {   
+            //for (int i = 0; i <= 45; i++)
+            //{
+            //    ClienteViewModel novo = new ClienteViewModel();
+            //    novo.Nome = "Paulo " + i;
+            //    novo.Id = 1+i;
+            //    novo.Telefone = "2456098";
+            //    lista.Add(novo);
+            //}
+
+            return View(lista);
         }
+
+
+        [HttpPost]
+        public IActionResult SalvarDados(ClienteViewModel model) 
+        {
+            if (model.Id > 0)
+            {
+                int indice = lista.FindIndex(a => a.Id == model.Id);
+                lista[indice] = model;
+            }
+            else 
+            {
+                Random radom = new Random();
+                model.Id = radom.Next(1, 9999);
+                lista.Add(model);
+            }
+
+            
+            lista.Add(model);
+            return RedirectToAction("Lista");
+            
+        }
+
 
         public IActionResult Cadastro()
         {
@@ -32,12 +67,25 @@ namespace webApp_manha.Controllers
 
         public IActionResult Editar(int id)
         {
-            return View();
+            ClienteViewModel cliente = lista.Find(a => a.Id == id);
+            if (cliente != null)
+            {
+                return View(cliente);
+            }
+            else {
+                return RedirectToAction("lista");
+            }
         }
 
         public IActionResult Excluir(int id)
         {
-            return View();
+            ClienteViewModel cliente = lista.Find(a => a.Id == id);
+            if (cliente != null) 
+            {
+                lista.Remove(cliente);
+            }
+            return RedirectToAction("Lista");
+
         }
 
         public IActionResult Comprass(int id)
---
+++ webApp_manha/Models/ClienteViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace webApp_manha.Models
+{
+    public class ClienteViewModel
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; } 
+    }
+}
